56e0cb1dd486f483915c818c053d033c84fae3c1
mojonapower <maca95@gmail.com>
2022-08-26 21:01:28 +0000

    tarea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { Navbar } from "./navbar.jsx";
+import {Accordion} from "./Accordion.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+//los componentes son funciones que retornan html
+//todas funciones retornan un valor
 //create your first component
 const Home = () => {
 	return (
+	
 		<div className="text-center">
+			<Navbar/>
+			<Accordion/>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -19,6 +24,7 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+			
 		</div>
 	);
 };
